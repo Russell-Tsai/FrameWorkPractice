51eabe478da4b2e52dd8b6c72bd767ba9fe81d21
RUSSELL.TSAI <vanilla.love10@gmail.com>
2017-12-08 22:41:36 +0800

    Finishied our FrameWork

---
+++ FrameWorkPractice/FrameWorkPractice/FrameWorkPractice.h
@@ -0,0 +1,19 @@
+//
+//  FrameWorkPractice.h
+//  FrameWorkPractice
+//
+//  Created by 蔡松樺 on 08/12/2017.
+//  Copyright © 2017 蔡松樺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameWorkPractice.
+FOUNDATION_EXPORT double FrameWorkPracticeVersionNumber;
+
+//! Project version string for FrameWorkPractice.
+FOUNDATION_EXPORT const unsigned char FrameWorkPracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorkPractice/PublicHeader.h>
+
+
---
+++ FrameWorkPractice/FrameWorkPractice/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  FrameWorkPractice
+//
+//  Created by 蔡松樺 on 08/12/2017.
+//  Copyright © 2017 蔡松樺. All rights reserved.
+//
+
+import Foundation
+
+
+public class service {
+    
+    private init(){}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff, "
+    }
+}
